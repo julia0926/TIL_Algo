4a1dcb830f926f3c7d30220f03daa8fd687c4285
kimjiin <kj980926@naver.com>
2021-08-29 14:24:46 +0900

    이항 계수 2

--- 다이나믹/11051.py
+++ 다이나믹/11051.py
@@ -1,4 +1,4 @@
-n, k = list(map(int, input().split()))
+n, k = map(int, input().split())
 dp = [1 for _ in range(n+1)]
 
 if n==k and k==0: #nCn이나 nC0은 1이므로 
@@ -8,3 +8,9 @@ else:
     for i in range(2, n+1):
         dp[i] = dp[i-1] * i 
     print(dp[n] // (dp[k] * dp[n-k]) % 10007) #nCr 공식
+
+# 파이썬 내장모듈 factorial 사용 
+# from math import factorial
+# n, k = map(int, input().split())
+
+# print(factorial(n) // (factorial(k) * factorial(n-k)) % 10007)
