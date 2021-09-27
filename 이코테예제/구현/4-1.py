c7866ce52254408c557280c0439e14e5733ce4d4
kimjiin <kj980926@naver.com>
2021-09-27 15:55:10 +0900

    상하좌우

---
+++ 이코테예제/구현/4-1.py
@@ -0,0 +1,19 @@
+n = int(input())
+moves = input().split()
+
+x, y = 1, 1
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_type = ['L','R','U','D']
+
+for move in moves:
+    for i in range(4):
+        if move_type[i] == move:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    x, y = nx, ny
+
+print(x, y)
+
