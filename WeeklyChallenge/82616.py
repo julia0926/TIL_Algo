5fd3021e680dc325f95970edb785180f3a398250
kimjiin <kj980926@naver.com>
2021-11-19 15:10:44 +0900

    주석 추가

--- WeeklyChallenge/82616.py
+++ WeeklyChallenge/82616.py
@@ -1,10 +1,16 @@
-# https://programmers.co.kr/learn/courses/30/lessons/82612
+'''
+문제 :
+https://programmers.co.kr/learn/courses/30/lessons/82612
+
+참고하면 좋을 코드
+def solution(price, money, count):
+    return max(0,price*(count+1)*count//2-money)
+'''
 
 def solution(price, money, count):
     sum_num = 0
     for i in range(1,count+1):
-        new = price * i
-        sum_num += new
+        sum_num += price * i
     if sum_num > money:
         return sum_num - money
     else:
