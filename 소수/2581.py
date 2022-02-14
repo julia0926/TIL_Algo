bda6ef92520fdfc9873bd496a28200660077ad6a
kimjiin <kj980926@naver.com>
2022-02-14 19:13:15 +0900

    소수 (에라토스의 체 사용안함)

--- 소수/2581.py
+++ 소수/2581.py
@@ -1,23 +1,18 @@
 n = int(input())
 m = int(input())
 
-a = [False, False] + [True] * (m-1)
 primes = []
 
-for i in range(2, m+1):
-    if a[i]: primes.append(i)
-    for j in range(2*i, m+1, i):
-        a[j] = False
-
-sum_v = 0
-min_v = 1e9
 for i in range(n, m+1):
-    if i in primes:
-        sum_v += i
-        min_v = min(min_v, i)
+    cnt = 1
+    for j in range(2, i):
+        if i % j == 0:
+            cnt = 0
+    if cnt and i != 1:
+        primes.append(i)
 
-if sum_v == 0:
-    print(-1)
+if primes:
+    print(sum(primes))
+    print(min(primes))
 else:
-    print(sum_v)
-    print(min_v)    
+    print(-1)
