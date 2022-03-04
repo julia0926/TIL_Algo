1b66178a4eb955664e90713a12ccdc5169a4d643
kimjiin <kj980926@naver.com>
2022-03-04 13:37:00 +0900

    N과M 3,4번

---
+++ 백트래킹/N과M/3번_15651.py
@@ -0,0 +1,17 @@
+# 3번 N과 M : https://www.acmicpc.net/problem/15651
+
+
+n, m = map(int, input().split())
+result = []
+
+def dfs(depth):
+    if depth == m:
+        print(*result)
+        return
+    for i in range(1, n+1):
+        result.append(i)
+        dfs(depth+1)
+        result.pop()
+
+
+dfs(0)
