e8628e7bd4092b77b0110ab657b6971875daa05e
kimjiin <kj980926@naver.com>
2022-03-23 23:31:50 +0900

    쿼드트리

--- 트리/1992.py
+++ 트리/1992.py
@@ -11,10 +11,27 @@ area = [list(map(int, input().strip())) for _ in range(N)]
 재귀 시작전 [
 재귀 끝나면 ]
 '''
-half = (N // 2) - 1
-result = ""
+result = []
 
 def recursive(x, y, n):
     first = area[x][y] #재귀 첫번째 값 
-    for i in range(x):
-        for j in range(y):
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if first != area[i][j]:
+                half = n // 2
+                result.append('(')
+                recursive(x, y, half) #1사분면
+                recursive(x, y+half, half) #2사분면
+                recursive(x+half, y, half)#3사분면
+                recursive(x+half, y+half, half) #4사분면
+                result.append(')')
+                return
+    
+    if first == 0:
+        result.append('0')
+    else:
+        result.append('1')
+
+recursive(0, 0, N)
+for k in result:
+    print(k, end='')
