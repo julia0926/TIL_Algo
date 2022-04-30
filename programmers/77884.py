771ffaac0a9be0bb9fdaddecc07c5bfa029abfcd
kimjiin <kj980926@naver.com>
2022-04-30 10:33:13 +0900

    약수의 개수와 덧셈(추가 해답)

--- programmers/77884.py
+++ programmers/77884.py
@@ -1,4 +1,4 @@
-# 약수의 개수와 덧셈 : https://programmers.co.kr/learn/courses/30/lessons/77884
+약수의 개수와 덧셈 : https://programmers.co.kr/learn/courses/30/lessons/77884
 def calculate(value):
     divisors = []
     for i in range(1, value+1):
@@ -15,6 +15,19 @@ def solution(left, right):
         else:
             answer -= v
     return answer
+
+#간결한 풀이 
+def solution(left, right):
+    answer = 0
+    for i in range(left,right+1):
+        if int(i**0.5)==i**0.5: #제곱근이 있으면 홀수개이므로, ex) 4*4 이런 똑같은 수
+            print("i = ", i)
+            print(f'{int(i**0.5)}=={i**0.5}')
+            answer -= i
+        else:
+            answer += i
+    return answer
+
 
 print(solution(13, 17))
 print(solution(24, 27))
