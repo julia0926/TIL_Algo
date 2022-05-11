b8daba7d9f4743ba6f48d4af8b95f94ae1c95069
kimjiin <kj980926@naver.com>
2022-05-11 21:29:55 +0900

    집합으로 간결하게 해결

--- Dev-matching/2019_77484.py
+++ Dev-matching/2019_77484.py
@@ -20,6 +20,20 @@ def solution(lottos, win_nums):
         answer.append(7-common)
         return answer
 
+def solution_using_set(lottos, win_nums):
+    common = len(set(win_nums).intersection(set(lottos)))
+    zero = lottos.count(0)
+    rank = {
+        0: 6,
+        1: 6,
+        2: 5,
+        3: 4,
+        4: 3,
+        5: 2,
+        6: 1
+    }
+    return [rank[common+zero], rank[common]]
+    
 print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))
 print(solution([0, 0, 0, 0, 0, 0], [38, 19, 20, 40, 15, 25]))
 print(solution([45, 4, 35, 20, 3, 9], [20, 9, 3, 45, 4, 35]))
