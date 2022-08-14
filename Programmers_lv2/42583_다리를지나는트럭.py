9134fd18dae5c2614efe0e8530a14101ba2bc8ce
julia0926 <kj980926@naver.com>
2022-08-14 22:22:12 +0900

    [220814] 다리를 지나는 트럭
    다시 풀 것

---
+++ Programmers_lv2/42583_다리를지나는트럭.py
@@ -0,0 +1,22 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/42583
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    #weight 키로 이상이면 pop
+    secend = 0
+    truck_weights = deque(truck_weights)
+    cross = deque([0] * bridge_length) #다리에 있는 트럭 
+
+    while cross: #다리에 있는동안 
+        secend += 1
+        cross.popleft()
+        print(secend, cross)
+        if truck_weights:
+            if sum(cross) + truck_weights[0] <= weight:
+                cross.append(truck_weights.popleft())
+            else:
+                cross.append(0)
+    return secend
+
+
+print(solution(2, 10, [7,4,5,6]))
