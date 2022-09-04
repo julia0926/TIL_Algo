a1d96235b50015afab358d0bbe158884a6bac7a5
julia0926 <kj980926@naver.com>
2022-09-05 00:45:51 +0900

    [220905] 성격 유형 검사하기 (retry)

--- KAKAO/Level1/2022_인턴쉽_성격유형검사하기.py
+++ KAKAO/Level1/2022_인턴쉽_성격유형검사하기.py
@@ -2,20 +2,23 @@
 from collections import defaultdict
 
 def solution(survey, choices):
-    result = defaultdict(int)
-    person_list = [('R','T'), ('C','F'), ('J','M'), ('A','N')]
+    personal_type = defaultdict(int)
+
     for s, c in zip(survey, choices):
         if c < 4:
-            result[s[0]] += 4 - c
+            personal_type[s[0]] += 4 - c
         else:
-            result[s[1]] += c - 4
-    answer = ""
+            personal_type[s[1]] += c - 4
+    person_list = [('R','T'), ('C','F'), ('J','M'), ('A','N')]
+    answer = ''
     for person in person_list:
-        if result[person[0]] >= result[person[1]]:
+        print(personal_type[person[0]],personal_type[person[1]] )
+        if personal_type[person[0]] > personal_type[person[1]] or personal_type[person[0]] == personal_type[person[1]]:
             answer += person[0]
-        else:
-            answer += person[1]
+        elif personal_type[person[0]] < personal_type[person[1]]:
+            answer += person[1] 
+
     return answer
 
-# print(solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5]))
+print(solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5]))
 print(solution(["CF", "FC", "CF"], [7,1,3]))
