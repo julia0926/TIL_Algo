883967601c8602396247f5a125f2d1eb2e801b67
julia0926 <kj980926@naver.com>
2022-09-13 19:09:25 +0900

    [220913] 모음사전

---
+++ 월간코드/모음사전.py
@@ -0,0 +1,20 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/84512
+
+def dfs(x, tmp_list, alpha):
+    print(tmp_list)
+    tmp_list.append(x)
+    for i in alpha:
+        if len(x) != len(alpha):
+            dfs(x+i, tmp_list, alpha)
+        else:
+            x[:-1]
+
+def solution(word):
+    alpha = ['A', 'E', 'I', 'O', 'U']
+    tmp_list = []
+    for value in alpha:
+        dfs(value, tmp_list, alpha)
+    return tmp_list.index(word) + 1
+
+
+print(solution("I"))
