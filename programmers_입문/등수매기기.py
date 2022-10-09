792dfcca0d24737f3c19386bcd44623037a8296a
julia0926 <kj980926@naver.com>
2022-10-09 19:41:50 +0900

    [221009] 등수매기기

---
+++ programmers_입문/등수매기기.py
@@ -0,0 +1,23 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120882
+
+from collections import defaultdict
+from copy import deepcopy
+from email.policy import default
+
+def solution(score):
+    ev = []
+    for a, b in score:
+        ev.append((a+b)//2)
+    ev_dic = defaultdict(int)
+    for idx, value in enumerate(sorted(ev, reverse=True)):
+        if ev_dic[value] == 0:
+            ev_dic[value] = idx+1
+    result = []
+    for e in ev:
+        result.append(ev_dic[e])
+    return result
+
+    
+
+solution([[80, 70], [90, 50], [40, 70], [50, 80]])
+solution([[80, 70], [70, 80], [30, 50], [90, 100], [100, 90], [100, 100], [10, 30]])
