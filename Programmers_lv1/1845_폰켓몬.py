73820db2b4adbdb36651e759a556137e07cd5a54
julia0926 <kj980926@naver.com>
2022-11-04 22:08:41 +0900

    [221104] 폰켓몬 (수정)

--- Programmers_lv1/1845_폰켓몬.py
+++ Programmers_lv1/1845_폰켓몬.py
@@ -1,17 +1,19 @@
-from itertools import combinations
+#https://school.programmers.co.kr/learn/courses/30/lessons/1845
 
 def solution(nums):
-    combi = set(combinations(nums,len(nums)//2))
-    answer = 0
-    for c in combi:
-        tmp = []
-        count = 0
-        for j in c:
-            if j not in tmp:
-                tmp.append(j)
-                count += 1
-        answer = max(answer, count)
-    return answer
+    nums_len = len(nums)
+    set_len = len(set(nums))
+    diff = nums_len - (nums_len - set_len)
+    print(diff)
+    if diff > len(nums)//2:
+        return len(nums)//2
+    else:
+        return diff
 
-# solution([3,1,2,3])
+#개간결..!
+def solution2(nums):
+    return min(len(nums)/2, len(set(nums)))
+
+solution([3,1,2,3])
+solution([3,3,3,2,2,4])
 solution([3,3,3,2,2,2]	)
