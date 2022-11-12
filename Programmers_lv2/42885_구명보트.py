3edf5596c3e48ff1fef8e8906535983cb4ff1b12
julia0926 <kj980926@naver.com>
2022-11-12 17:37:41 +0900

    [221112] 구명보트

--- Programmers_lv2/42885_구명보트.py
+++ Programmers_lv2/42885_구명보트.py
@@ -1,8 +1,17 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42885
 
-# 투포인터 공부후 풀어보기
-# 참고 : https://school.programmers.co.kr/questions/32050
+from collections import deque
 
-    
-solution([70, 50, 80, 50], 100)
-# solution([70, 80, 50], 100)
+def solution(people, limit):
+    answer = 0
+    dq = deque(sorted(people))
+    while dq:
+        if len(dq) == 1:
+            answer += 1
+            break
+        if dq[0] + dq[-1] <= limit: #같이 탈수있다면
+            dq.popleft() 
+        dq.pop()
+        answer += 1
+
+    return answer
