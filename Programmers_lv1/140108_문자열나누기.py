3a6bead380f9ea9a8f10624a50e965fdda4a1b07
julia0926 <kj980926@naver.com>
2022-12-05 16:54:31 +0900

    [221205] 문자열 나누기

--- Programmers_lv1/140108_문자열나누기.py
+++ Programmers_lv1/140108_문자열나누기.py
@@ -1,20 +1,37 @@
-#https://school.programmers.co.kr/learn/courses/30/lessons/140108#
-from collections import defaultdict 
+# https://school.programmers.co.kr/learn/courses/30/lessons/140108
 
-#1차시도 -> 28.6
-def solution_(s):
-    dic = defaultdict(int)
+'''
+문제를 잘못 이해하고, 문자열 각각의 개수를 세는줄 알았다.
+처음 문자열 / 그외 문자열의 개수를 비교하는 문제였음
+'''
+
+
+def solution(s):
+    first = 0
+    diff = 0
+    piv = s[0]
     answer = 0
-    for ch in s:
-        dic[ch] += 1
-        dic_len = len(dic.values())
-        if dic_len > 1 and dic_len != len(set(dic.values())):
+
+    for i in range(len(s)):
+        if piv == s[i]:
+            first += 1
+        else:
+            diff += 1
+        
+        if first == diff and first != 0:
             answer += 1
-            # print(dic)
-            dic = defaultdict(int)
-    if dic:
+            
+            if i + 1 < len(s):
+                piv = s[i+1]
+            first, diff = 0, 0
+
+    if first != 0:
         answer += 1
+
+        
+
+    
     return answer
 
-# solution("aaabbaccccabba")
 solution("abracadabra")
+solution("aaabbaccccabba")
