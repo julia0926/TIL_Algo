1972b1ab754698ddd784433a5b26dd71ee7a9fc0
julia0926 <kj980926@naver.com>
2023-01-02 09:01:04 +0900

    [220102] Z (시간초과 해결)

--- Backjoon/재귀/1074.py
+++ Backjoon/재귀/1074.py
@@ -1,18 +1,16 @@
 n, r, c = map(int, input().split()) #2^n개 중 r열 c행
-ans = 0
-def recursive(x, y, n):
-    global ans
-    if x == r and y == c:
-        print("@@", ans)
-        exit(0)
-    if n == 1: #재귀의 끝까지 도달했으면
-        print(x, y, ans)
-        ans += 1
-        return
-    half = n // 2
-    recursive(x, y, half) #1사분면
-    recursive(x+half, y, half) #2
-    recursive(x, y+half, half) #3
-    recursive(x+half, y+half, half) #4
 
-recursive(0, 0, 2 **n)
+def recursive(n, x, y):
+    if n == 0:  return 0
+    half = pow(2, n-1)
+    if x < half and y < half: #1사분면 
+        return recursive(n-1, x, y)
+    if x < half and y >= half: #2사분면 
+        return half*half + recursive(n-1, x, y-half) #시작점을 더하고 + y값을 갱신 
+    if x >= half and y < half: #3사분면 
+        return 2*half*half + recursive(n-1,x-half, y)
+    if x >= half and y >= half:
+        return 3*half*half + recursive(n-1,x-half,y-half)
+
+#시작점을 갱신시키면서 분할정복
+print(recursive(n, r, c)) 
