62e2c21541bdca58b300af5902e79026944df207
julia0926 <kj980926@naver.com>
2023-01-03 11:33:36 +0900

    [220103] N과M (11)

---
+++ Backjoon/백트래킹/N과M_230103/11번_15665.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+arr = sorted(list(map(int, input().split())))
+result = []
+
+def dfs():
+    #base condition
+    if len(result) == m:
+        print(*result)
+        return
+    before = 0
+    for i in range(n):
+        if before != arr[i]:
+            result.append(arr[i])
+            before = arr[i]
+            dfs()
+            result.pop()
+
+
+dfs()
