d1174406162eec8fcfe5def19affa1ddc47651ec
julia0926 <kj980926@naver.com>
2023-01-03 11:14:16 +0900

    [220103] N과M (4)

---
+++ Backjoon/백트래킹/N과M_230103/4번_15652.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+result = []
+
+def dfs(v):
+    if len(result) == m:
+        print(*result)
+        return
+    for i in range(v, n+1):
+        result.append(i)
+        dfs(i)
+        result.pop()
+            
+
+dfs(1)
