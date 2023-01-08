4f1714901a55c2390e9856b93bbcddfc3d1e6a6e
julia0926 <kj980926@naver.com>
2023-01-08 21:29:04 +0900

    [220108] 감시 (미완성) 풀이중: 경우의수 계산

--- Backjoon/시뮬레이션/boj15683.py
+++ Backjoon/시뮬레이션/boj15683.py
@@ -6,36 +6,38 @@
 cctv 통과함 
 사각지대 : 감시할 수 없는 영역 
 '''
-from collections import deque
-
-#경우의 수 구하기
 
 n, m = map(int, input().split()) #세로, 가로
 answer = 100 #최대 
 cctv, graph = [], []
+cctv_dir = []
+direction = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+observer_dir = [0,4,2,4,4,1]
 for i in range(n):
     graph.append(list(map(int, input().split())))
     for j in range(m):
         if 1<=graph[i][j]<=5:
-            cctv.append((i, j))
-direction = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+            cctv.append((graph[i][j]))
+            cctv_dir.append(observer_dir[graph[i][j]])
+print(cctv_dir)
 
-print(len(cctv))
-temp = []
-
-
-#CCTV 개수에 따른 경우의 수를 구한다. → 백트래킹으로 
-#미완성 코드, 내일 다시도전!
-def dfs(depth):
-    if len(cctv) == depth:
-        print(temp)
+def dfs(x, ans):
+    if x!=0 and not ans:
         return
-    for i in range(len(cctv)):
-        temp.append(i)
-        dfs(depth+1)
-        temp.pop()
-    
-dfs(0)
+    if x == len(cctv):
+        print(ans)
+        return
+    for i in range(cctv_dir[x]):
+        ans.append(i)
+        dfs(x+1, ans)
+        ans.pop()
+dfs(0, [])
+'''
+길이 = 3
+[2,2,1]
+0,0,0
+0,1,0
+1,0,0
+1,1,0
 
-
-
+'''
