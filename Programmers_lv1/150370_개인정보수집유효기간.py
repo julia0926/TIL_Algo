6e74e5d3a5c9a774472012af0d16eac9e6aa50c2
julia0926 <kj980926@naver.com>
2023-01-13 10:42:28 +0900

    [230113] 2023 KAKAO BLIND (성공) 개인정보 수집 유효기간

--- Programmers_lv1/150370_개인정보수집유효기간.py
+++ Programmers_lv1/150370_개인정보수집유효기간.py
@@ -1,36 +1,25 @@
-from datetime import datetime
+# 년월일로 계산하는 것이 아닌 day로 다 계산해서 풀어서 구해야함 
+
 def solution(today, terms, privacies):
     now_year, now_month, now_day = map(int, [today[:4], today[5:7], today[-2:]])
     term_dic = {}
     for t in terms:
         sort, valid_date = t.split()
-        term_dic[sort] = int(valid_date)
-    
-    def calculate_date(date, type):
-        p_year, p_month, p_day = map(int, date.split('.'))
-        p_month += term_dic[type]
-        if p_month > 12:
-            p_year += 1
-            p_month -= term_dic[type]+1
-        p_day -= 1
-        if p_day == 0:
-            p_day = 28
-        return p_year, p_month, p_day
+        term_dic[sort] = int(valid_date)      
 
-    #현재보다 이후인지 확인 
-    def validate_date(y, m, d):
-        compare_date = datetime(y, m, d)
-        now_date = datetime(now_year, now_month, now_day)
-        return True if compare_date >= now_date else False
+    def today_cal(date):
+        p_year, p_month, p_day = map(int, date.split('.'))
+        return ((p_year-2000) * 12 * 28) + p_month * 28 + p_day
 
     result = []
+    today_diff = today_cal(today)
     for idx, p in enumerate(privacies):
         date, type = p.split()
-        y, m, d = calculate_date(date, type)
-        if not validate_date(y, m, d): #보관 가능하다면
+        if today_cal(date) + term_dic[type]*28 <= today_diff:
             result.append(idx+1)
 
     return result
 
-
-solution("2022.05.19", ["A 6", "B 12", "C 3"], ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"])
+# print(solution("2022.05.19", ["A 6", "B 12", "C 3"], ["2020.01.28 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]))
+print(solution("2022.05.19", ["A 6", "B 12", "C 3"], ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]))
+print(solution("2020.01.01", ["Z 3", "D 5"], ["2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"]))
