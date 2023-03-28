3d3b34d6e6467346a206a2b7d0814278ddd8dff0
ji_in <37897873+julia0926@users.noreply.github.com>
2023-03-28 11:01:47 +0900

    [level 2] Title: 순위 검색, Time: 539.18 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
@@ -1,37 +1,29 @@
-from collections import defaultdict
-from itertools import combinations
 from bisect import bisect_left
 
-def solution(infomation, queries):
-    info_dic = defaultdict(list)
-    for info in infomation:
-        info = info.split()
-        condition = info[:-1]
-        score = int(info[-1])
-        for i in range(5): #-를 넣는 경우를 계산하는 거군..
-            case = list(combinations([0,1,2,3],i))
-            for c in case:
-                tmp = condition.copy()
-                for idx in c:
-                    tmp[idx] = "-"
-                key = ''.join(tmp)
-                info_dic[key].append(score)
-    #점수 오름차순
-    for value in info_dic.values():
-        value.sort()
-    result = []
-    for query in queries:
-        query = query.replace(" and ", "")
-        query = query.split()
-        target_key = ''.join(query[:-1])
-        target_score = int(query[-1])
-        count = 0
-        if target_key in info_dic:
-            target_list = info_dic[target_key]
-            idx = bisect_left(target_list, target_score)
-            # print(target_list, target_score, idx)
-            count = len(target_list) - idx
-        result.append(count)
-    # print(result)
+def solution(info, query):
+    data = dict()
+    for a in ['cpp', 'java', 'python', '-']:
+        for b in ['backend', 'frontend', '-']:
+            for c in ['junior', 'senior', '-']:
+                for d in ['chicken', 'pizza', '-']:
+                    data.setdefault((a, b, c, d), list())
 
-    return result
+    for i in info:
+        i = i.split()
+        for a in [i[0], '-']:
+            for b in [i[1], '-']:
+                for c in [i[2], '-']:
+                    for d in [i[3], '-']:
+                        data[(a, b, c, d)].append(int(i[4]))
+    for k in data:
+        data[k].sort()
+
+    answer = []
+    for q in query:
+        count = 0
+        q = q.split()
+        pool = data[(q[0], q[2], q[4], q[6])]
+        find = int(q[7])
+        idx = bisect_left(pool, find)
+        answer.append(len(pool)-idx)
+    return answer
