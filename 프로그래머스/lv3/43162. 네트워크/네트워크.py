794bac817505a36c1454c9f229530ad7a6fc63d0
ji_in <37897873+julia0926@users.noreply.github.com>
2023-04-04 14:13:36 +0900

    [level 3] Title: 네트워크, Time: 5.19 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,26 +1,10 @@
 def solution(n, computers):
-    connected = list([0] * n for _ in range(n))
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                if computers[i][j] == 1 or (computers[i][k] == 1 and computers[k][j] == 1):
-                    connected[i][j] = 1
-    visited = [False] * n
-    total = n-1
-    def dfs(x):
-        visited[x] = True
-        stack = [x]
-        while stack:
-            v = stack.pop()
-            for i in range(n):
-                if i != v and not visited[i] and connected[i][v] == connected[v][i] == 1:
-                    # print(v,i, connected[i][total-i],connected[total-i][i]  )
-                    stack.append(i)
-                    visited[i] = True
-    result = 0
+    temp = [i for i in range(n)]
     for i in range(n):
-        if not visited[i]:
-            dfs(i)
-            result += 1
-    # print(result)
-    return result
+        for j in range(n):
+            if computers[i][j]: #연결 됐다면, 1이라면
+                for k in range(n):
+                    if temp[k] == temp[i]: #이웃한 것끼리 연결되어있다면
+                        print(temp[k] , temp[i],  temp[j])
+                        temp[k] = temp[j] #이웃한 노드에도 연결상태를 부여한다.
+    return len(set(temp))
