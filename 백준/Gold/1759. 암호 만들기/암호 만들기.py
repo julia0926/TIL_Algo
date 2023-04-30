e2102bc941473f9e237f49507d7b40dc4a3b385d
ji_in <37897873+julia0926@users.noreply.github.com>
2023-04-30 11:17:08 +0900

    [Gold V] Title: 암호 만들기, Time: 124 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,18 +1,15 @@
+from itertools import combinations
+
 n, m = map(int, input().split())
 arr = sorted(list(input().split()))
 vowels = ['a','e','i','o','u']
 result = []
-def dfs(start):
-    if len(result) == n:
-        count = sum(result.count(v) for v in vowels)
-        if count >= 1 and n - count >= 2:
-            print(''.join(result))
-            return
-    for i in range(start, m):
-        if arr[i] not in result: #중복 제거
-            #모음이 최소 한개
-            result.append(arr[i])
-            dfs(i+1)
-            result.pop()
 
-dfs(0)
+for c in combinations(sorted(arr), n):
+    result.append(''.join(c))
+
+#1개 모음, 2개 자음 거르기
+for res in result:
+    cnt = sum(res.count(v) for v in vowels)
+    if cnt >= 1 and n - cnt >= 2:
+        print(res)
