54459ba1d16dac4b6ee2f245fa76ff1a1454dff3
ji_in <37897873+julia0926@users.noreply.github.com>
2023-05-03 23:07:15 +0900

    [Bronze III] Title: 사격 내기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/27960. 사격 내기/사격 내기.py
+++ 백준/Bronze/27960. 사격 내기/사격 내기.py
@@ -1,17 +1,6 @@
 a, b = map(int, input().split())
 
 result = ''
-a, b = format(a, 'b'), format(b, 'b')
-max_len = max(len(a), len(b))
-a = a.zfill(max_len)
-b = b.zfill(max_len)
 
-
-for i, j in zip(a, b):
-    if i != j:
-        result += '1'
-    else:
-        result += '0'
-
-print(int(result, 2))
+print(a ^ b)
 
