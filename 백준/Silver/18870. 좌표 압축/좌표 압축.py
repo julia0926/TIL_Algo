cbad7d7464776856cce8aca0bbc63568f28c5cd4
ji_in <37897873+julia0926@users.noreply.github.com>
2023-05-15 10:34:54 +0900

    [Silver II] Title: 좌표 압축, Time: 1416 ms, Memory: 268272 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+from bisect import bisect_left
+n = int(input())
+arr = list(map(int, input().rstrip().split()))
+sort_arr = sorted(arr)
+#중복제거
+new_arr = [sort_arr[0]]
+for i in range(1, n):
+    if new_arr[-1] != sort_arr[i]:
+        new_arr.append(sort_arr[i])
+
+result = []
+for val in arr:
+    n = bisect_left(new_arr, val)
+    result.append(n)
+print(*result)
