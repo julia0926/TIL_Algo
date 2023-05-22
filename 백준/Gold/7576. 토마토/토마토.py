24c66306f68f4296fd48930a1e87300fe375818c
ji_in <37897873+julia0926@users.noreply.github.com>
2023-05-22 12:09:53 +0900

    [Gold V] Title: 토마토, Time: 524 ms, Memory: 167024 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,36 +1,38 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-n, m = map(int, input().strip().split())
-dq = deque()
+m, n = map(int, input().rstrip().split())
 graph = []
-for i in range(m):
-    g = list(map(int, input().strip().split()))
-    for j in range(n):
-        if g[j] == 1:
-            dq.append((i, j))
-    graph.append(g)
+dq = deque()
+for i in range(n):
+    gh = list(map(int, input().rstrip().split()))
+    for j in range(m):
+        if gh[j] == 1: dq.append((i, j))
+    graph.append(gh)
+
+#output : 모두 익을 때 최소 날짜 
 
 def bfs():
-    visited = [[False] * n for _ in range(m)]
     direction = [(0,1),(0,-1),(1,0),(-1,0)]
-
+    visited = [[False] * m for _ in range(n)]
+    
     while dq:
         a, b = dq.popleft()
         visited[a][b] = True
         for dx, dy in direction:
             nx = a + dx
             ny = b + dy
-            if 0<=nx<m and 0<=ny<n and not visited[nx][ny] and graph[nx][ny] == 0:
-                dq.append((nx,ny))
+            
+            if 0<=nx<n and 0<=ny<m and graph[nx][ny] == 0 and not visited[nx][ny]:
+                dq.append((nx, ny))
                 graph[nx][ny] = graph[a][b] + 1
-
 bfs()
-result = 0
+res = 0
 for g in graph:
     if 0 in g:
         print(-1)
         exit(0)
-    result = max(result, max(g))
-print(result-1)
+    res = max(res, max(g))
+
+print(res-1)
