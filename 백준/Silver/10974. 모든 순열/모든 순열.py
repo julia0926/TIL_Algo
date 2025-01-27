3dc21d5622bda32cd9b7f01eeee8b3e65aa0a025
Jiin Kim <37897873+julia0926@users.noreply.github.com>
2025-01-27 18:36:14 +0900

    [Silver III] Title: 모든 순열, Time: 164 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.py
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -1,13 +1,21 @@
 n = int(input())
-arr = []
-def dfs():
-    if len(arr) == n:
-        print(*arr)
-        return
-    for i in range(1, n+1):
-        if i not in arr:
-            arr.append(i)
-            dfs()
-            arr.pop()
+arr = [i for i in range(1, n+1)]
+check = [False] * n
+choose = []
 
-dfs()
+def permutation(level):
+    if level == n:
+        print(*choose)
+        return
+    
+    for i in range(n):
+        if check[i]: #인덱스 i가 이미 쓰이고 있으면 무시
+            continue
+        choose.append(arr[i])
+        check[i] = True
+        permutation(level+1)
+
+        check[i] = False
+        choose.pop()
+
+permutation(0)
