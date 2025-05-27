cb41a56d79d6669cc0e53062d57959115a4a0b0b
Jiin Kim <37897873+julia0926@users.noreply.github.com>
2025-05-27 17:05:58 +0900

    [Gold V] Title: 토마토, Time: 396 ms, Memory: 163832 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,38 +1,39 @@
-import sys
-input = sys.stdin.readline
 from collections import deque
 
-m, n = map(int, input().rstrip().split())
-graph = []
-dq = deque()
-for i in range(n):
-    gh = list(map(int, input().rstrip().split()))
-    for j in range(m):
-        if gh[j] == 1: dq.append((i, j))
-    graph.append(gh)
+m, n = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-#output : 모두 익을 때 최소 날짜 
-
-def bfs():
-    direction = [(0,1),(0,-1),(1,0),(-1,0)]
-    visited = [[False] * m for _ in range(n)]
-    
+def bfs(tomatos):
+    dq = deque(tomatos)
     while dq:
-        a, b = dq.popleft()
-        visited[a][b] = True
-        for dx, dy in direction:
-            nx = a + dx
-            ny = b + dy
-            
-            if 0<=nx<n and 0<=ny<m and graph[nx][ny] == 0 and not visited[nx][ny]:
+        x, y = dq.popleft()
+        for dx, dy in [(0, 1), (1, 0 ), (-1, 0), (0, -1)]:
+            nx = x + dx
+            ny = y + dy
+            if 0<=nx<n and 0<=ny<m and graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
                 dq.append((nx, ny))
-                graph[nx][ny] = graph[a][b] + 1
-bfs()
-res = 0
-for g in graph:
-    if 0 in g:
-        print(-1)
-        exit(0)
-    res = max(res, max(g))
 
-print(res-1)
+tomatos = []
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
+            tomatos.append((i, j))
+
+bfs(tomatos)
+
+
+
+def solution():
+    result = 0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 0:
+                return -1
+        result = max(result, max(graph[i]))
+    if result == 1:
+        return 0
+    else:
+        return result-1
+
+print(solution())
